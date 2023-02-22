7bc394f190bb39800632d4a28030e1a608995ffe
Doğukan Variş <57659479+do-Va@users.noreply.github.com>
2023-02-22 11:44:10 +0300

    Database created and connection established.

--- invoice-app/server.js
+++ invoice-app/server.js
@@ -1,10 +1,17 @@
 import express from 'express';
+import dotenv from 'dotenv';
+
+// database
+import connectDB from './database/connect.js';
 
 // middleware
 import errorHandlerMiddleware from './middleware/error-handler.js';
 import notFoundMiddleware from './middleware/not-found.js';
 
 const app = express();
+dotenv.config();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Welcome');
@@ -15,4 +22,16 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
