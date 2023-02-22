052dc8db0b92b5f8d08dda36c17ffde6e2d01ca3
Doğukan Variş <57659479+do-Va@users.noreply.github.com>
2023-02-22 10:11:01 +0300

    The context has been created.

--- invoice-app/client/src/index.js
+++ invoice-app/client/src/index.js
@@ -4,12 +4,15 @@ import 'normalize.css';
 
 import App from './App';
 import GlobalStyles from './styles/GlobalStyles';
+import { AppProvider } from './context/appContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppProvider>
+      <App />
 
-    <GlobalStyles />
+      <GlobalStyles />
+    </AppProvider>
   </React.StrictMode>
 );
