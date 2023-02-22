32e5fb527ce72d80110bf49b23657a6206d5d2ef
Doğukan Variş <57659479+do-Va@users.noreply.github.com>
2023-02-22 10:55:25 +0300

    Added not found and error handler middleware.

--- invoice-app/server.js
+++ invoice-app/server.js
@@ -1,10 +1,17 @@
 import express from 'express';
 
+// middleware
+import errorHandlerMiddleware from './middleware/error-handler.js';
+import notFoundMiddleware from './middleware/not-found.js';
+
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Welcome');
 });
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
