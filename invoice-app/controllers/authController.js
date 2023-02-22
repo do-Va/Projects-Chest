1dc259fefb5d353c3784d86f48e83ba70a04cdec
Doğukan Variş <57659479+do-Va@users.noreply.github.com>
2023-02-22 13:19:11 +0300

    Added jwt package and added token generating function.

--- invoice-app/controllers/authController.js
+++ invoice-app/controllers/authController.js
@@ -18,8 +18,11 @@ const register = async (req, res) => {
 
   const user = await User.create({ name, email, password });
 
+  const token = user.createJWT();
+
   res.status(StatusCodes.CREATED).json({
     user: { email: user.email, name: user.name },
+    token,
   });
 };
 
--- invoice-app/model/User.js
+++ invoice-app/model/User.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 import validator from 'validator';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -33,5 +34,12 @@ UserSchema.pre('save', async function () {
 
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+// Generate token
+UserSchema.methods.createJWT = function () {
+  return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+};
 
 export default mongoose.model('User', UserSchema);
