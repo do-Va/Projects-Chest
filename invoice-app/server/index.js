c5968d3d3fa34a0256e2c89e9bb9aa707a5c5917
Doğukan Variş <57659479+do-Va@users.noreply.github.com>
2023-03-13 14:16:47 +0300

    To deploy the project, the server side was added to a folder named server.

--- invoice-app/server/index.js
+++ invoice-app/server/index.js
@@ -2,9 +2,6 @@ import 'express-async-errors';
 import express from 'express';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import path from 'path';
 
 // database
 import connectDB from './database/connect.js';
@@ -25,21 +22,16 @@ if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
 }
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-// only when ready to deploy
-app.use(express.static(path.resolve(__dirname, './client/build')));
-
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome');
+});
 
 const base = '/api/v1';
 
 app.use(`${base}/auth`, authRouter);
 app.use(`${base}/invoices`, authenticateUser, invoiceRouter);
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
-});
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
